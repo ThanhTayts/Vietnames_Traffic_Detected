25a7946570a99b616f93ff7d9a227484cbe2a7fd
ThanhTayts <92384494+ThanhTayts@users.noreply.github.com>
2022-05-05 16:03:04 +0700

    Add files via upload

---
+++ download_from_google.py
@@ -0,0 +1,6 @@
+from simple_image_download import simple_image_download as simp
+
+response = simp.simple_image_download
+
+response().download(keywords = "biển báo đường người đi bộ" ,limit = 100 )
+# Download hình ảnh biển báo người đi bộ từ Google , giới hạn là 100 ảnh
